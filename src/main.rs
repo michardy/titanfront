e4d84f35f416abefc86b6ca7c6bfdafb8818d460
Michael Hardy <98.michael.hardy@gmail.com>
2021-12-31 19:11:42 -0800

    Further progress on UDP proxying
    Reversing needed to find auth token position

--- src/main.rs
+++ src/main.rs
@@ -27,19 +27,60 @@ pub struct AuthTables {
 
 pub type Request = tide::Request<AuthTables>;
 
-fn relay_incoming(auth_tables: &AuthTables) {
+const PACKET_MAX: usize = 2048;
+const PROXY_ADDR: &'static str = "10.255.0.1:37015";
+const INTERNAL_INTERFACE: &'static str = "10.255.0.1:0";
+const DESTINATION: &'static str = "10.255.1.1:37015";
 
+fn relay_incoming(proxy_sock: &UdpSocket, auth_tables: &AuthTables) {
+	let mut buf: [u8;2048] = [0u8;PACKET_MAX];
+	match proxy_sock.recv_from(&mut buf) {
+		Ok((size, addr)) => {
+			// TODO: Determine if this is a no op
+			if size > PACKET_MAX {
+				log::warn!(
+					"Dropped packet of size {}, greater than {}",
+					size,
+					PACKET_MAX
+				);
+				return
+			}
+			match auth_tables.ips.get(&addr) {
+				Some(sock) => {
+					match sock.send(&buf) {
+						Ok(_) => {},
+						Err(e) => log::warn!("Could not relay packet {}", e),
+					}
+				},
+				None => {
+
+				}
+			}
+		},
+		Err(_) => {
+			log::warn!("Received packet from unknown address")
+		}
+	}
 }
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
+	env_logger::init();
+
 	// Setup UDP relaying sockets
-	let proxy_sock = UdpSocket::bind("0.0.0.0:37015").expect("Configured proxy address could not be bound");
+	log::info!("Creating UDP sockets"); // log is not imported bc log::error would clobber error
+	let proxy_sock = UdpSocket::bind(PROXY_ADDR)
+		.expect("Configured proxy address could not be bound");
+
 	let mut internal_sockets: Vec<UdpSocket> = Vec::with_capacity(16);
 	for _ in 0..16 {
-		internal_sockets.push(UdpSocket::bind("0.0.0.0:0").expect("Cannot allocate socket for client relay"));
+		let sock = UdpSocket::bind(INTERNAL_INTERFACE)
+		.expect("Cannot allocate socket for client relay");
+		sock.connect(DESTINATION).expect("Could not connect relay to remote");
+		internal_sockets.push(sock);
 	}
 
+	log::info!("Setting up auth tables");
 	let auth_tables = AuthTables {
 		tokens: Arc::new(DashMap::new()),
 		ips: Arc::new(DashMap::new()),
@@ -48,7 +89,9 @@ async fn main() -> tide::Result<()> {
 	};
 
 	// Setup authserver
+	log::info!("Setting up auth server");
 	let mut authserver = tide::with_state(auth_tables);
+	log::info!("Starting auth server");
 	authserver.listen("0.0.0.0:8081").await?;
 	Ok(())
 }
